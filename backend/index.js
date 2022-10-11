e2f37428818817ab69651d8418254fc417420fca
antinglimmy <antinglim@gmail.com>
2022-10-11 21:24:18 +0800

    Added auth

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
-import express from "express";
+const express = require("express");
 
 const PORT = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
+const { auth } = require("express-oauth2-jwt-bearer");
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: "https://maplesell/api",
+  issuerBaseURL: `https://dev-hmluigxe.us.auth0.com`,
+});
+
+app.get("/", checkJwt, (req, res) => {
   res.send("Hello, World!");
 });
 
